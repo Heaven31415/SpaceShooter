2fbfdc12a0d15ec405442e03e4ee8571ffd323e0
Heaven31415 <Heaven31415@gmail.com>
2017-01-05 05:55:42 +0100

    Trying to fix formatting in ResourceHolder.hpp

--- Space Shooter Prototype 2016-09-01/Include/ResourceHolder.hpp
+++ Space Shooter Prototype 2016-09-01/Include/ResourceHolder.hpp
@@ -13,26 +13,26 @@ template<typename R>
 class ResourceHolder
 {
 public:
-									ResourceHolder(std::vector<std::string> extensions, std::string relativePath = "");
-	R&								get(const std::string& id);
-	const R&						get(const std::string& id) const;
-	float							getProgress();
-	bool							isFinished();
-	void							load();
+                                    ResourceHolder(std::vector<std::string> extensions, std::string relativePath = "");
+    R&								get(const std::string& id);
+    const R&						get(const std::string& id) const;
+    float							getProgress();
+    bool							isFinished();
+    void							load();
 
 private:
-	void							loadResources();
+    void							loadResources();
 
 private:
-	thor::ResourceHolder<R, std::string>	m_resources;
-	std::vector<std::string>				m_extensions;
-	std::string								m_relativePath;
+    thor::ResourceHolder<R, std::string>	m_resources;
+    std::vector<std::string>				m_extensions;
+    std::string								m_relativePath;
 
 private:
-	sf::Thread								m_loader;
-	std::atomic<size_t>						m_loaded;
-	std::atomic<size_t>						m_found;
-	std::atomic<bool>						m_finished;
+    sf::Thread								m_loader;
+    std::atomic<size_t>						m_loaded;
+    std::atomic<size_t>						m_found;
+    std::atomic<bool>						m_finished;
 };
 
 using FontHolder = ResourceHolder<sf::Font>;
